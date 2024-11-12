b421beadcd5afd36a8d7e07e767fcc8c3a875a04
AndreaNastruzzo <nastruzzoandrea@gmail.com>
2024-11-12 17:23:26 +0100

    .

---
+++ S2L2/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// ciao a tutti
+int main()
+{
+    int numero1;
+    int numero2;
+    int prodotto;
+
+    printf("calcoliamo il prodotto di due numeri\n");
+
+    printf("\nprimo numero");
+    scanf("%d",&numero1);
+
+    printf("\nsecondo numero");
+    scanf("%d",&numero2);
+
+    printf("il prodotto dei due numeri è %d", numero1*numero2 );
+
+
+}
